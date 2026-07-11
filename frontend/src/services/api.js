54a379cbc346311883ae39e712b7fc80ed5476ca
vikrampal12345 <vikrampal12345ram@gmail.com>
2026-07-11 18:43:49 +0530

    Connect frontend to Railway backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API = axios.create({
 
-    baseURL: "http://127.0.0.1:8000"
+    baseURL:  "https://resumejobrecommendationapplication-production-5c81.up.railway.app"
 
 });
 
